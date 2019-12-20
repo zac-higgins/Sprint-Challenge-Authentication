dee1e96f5c31f1154503344d58bb96b4a8d2a2f9
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-20 09:43:24 -0700

    Built out authentication and users endpoints. Answered self study questions

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: { filename: './database/auth.db3' },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
